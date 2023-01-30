0e48f5ffefecf3f96ef369adb15fd3dc710ce860
Sergey Babich <sergei.babich2022@yandex.by>
2023-01-31 00:45:35 +0300

    Add 125 refactor js files.

--- src/Codewars/JS2/125.js
+++ src/Codewars/JS2/125.js
@@ -1,6 +1,34 @@
-// This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
 
-var a = "code";
-var b = "wa.rs";
-var name = a + b;
+// Given the triangle of consecutive odd numbers:
+//
+//     1
+// 3     5
+// 7     9    11
+// 13    15    17    19
+// 21    23    25    27    29
+// ...
+// Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+//
+// 1 -->  1
+// 2 --> 3 + 5 = 8
 
+
+function rowSumOddNumbers(n) {
+
+    let counter1 =  1 //1723
+    let result = 2
+
+    for (let i = 1; i <n ; i++) {
+        counter1 += (result)
+        result +=2
+    }
+
+    let result2 = counter1
+    for (let i = 0; i < n-1; i++) {
+        counter1 += (result2 += 2)
+    }
+    return counter1
+    // return n*n*n
+}
+
+console.log(rowSumOddNumbers(42))

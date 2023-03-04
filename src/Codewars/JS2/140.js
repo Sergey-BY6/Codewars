1fb9e6408d98d5f26110c72093d014144b73e96d
Sergey Babich <sergei.babich2022@yandex.by>
2023-03-05 02:37:49 +0300

    Add 140 js.

---
+++ src/Codewars/JS2/140.js
@@ -0,0 +1,11 @@
+// Write a function that always returns 5
+//
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+//
+// Good luck :)
+
+
+function unusualFive() {
+    let a = ["a", "b", "c", "d", "e", "f"]
+    return a.indexOf("f")
+}

5fbdb2d87deaa2332e7458dd52cf794e86467dd5
Sergey Babich <sergei.babich2022@yandex.by>
2023-04-20 23:47:29 +0300

    Add 168 js.

---
+++ src/Codewars/JS2/168.js
@@ -0,0 +1,4 @@
+// Task:
+//     Write a function that will accept two parameters: variable and type and check if type of variable is matching type. Return true if types match or false if not.
+
+const typeValidation = (variable, type) => typeof variable === type

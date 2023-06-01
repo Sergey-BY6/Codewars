aab64bf39873edf1b0bbf576f3aa954a6167ab24
Sergey Babich <sergei.babich2022@yandex.by>
2023-06-02 00:10:59 +0300

    -Add 237-238 js.

---
+++ src/Codewars/JS3/237.js
@@ -0,0 +1,7 @@
+// Provided is a function Kata which accepts two parameters in the following order: array, element and returns the index of the element if found and "Not found" otherwise. Your task is to shorten the code as much as possible in order to meet the strict character count requirements of the Kata. (no more than 161) You may assume that all array elements are unique.
+
+
+const find = (a, b) => {
+    let c = a.indexOf(b)
+    return c === -1 ? "Not found" : c
+}

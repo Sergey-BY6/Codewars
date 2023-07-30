42435f4c909f384daeddb964e193ae099a30ffc7
Sergey Babich <sergei.babich2022@yandex.by>
2023-07-30 18:41:54 +0300

    Add 312 js.

---
+++ src/Codewars/JS4/312.js
@@ -0,0 +1,5 @@
+// Write a function that returns the total surface area and volume of a box as an array: [area, volume]
+
+
+const getSize = (width, height, depth) => [2 * (width * height + width * depth + height * depth), width * height * depth]
+

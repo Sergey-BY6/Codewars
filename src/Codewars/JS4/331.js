8701c5c0ce1a4f04c29aa3f30e4c56a310b49b57
Sergey Babich <sergei.babich2022@yandex.by>
2023-08-21 22:29:45 +0300

    Add 331 js.

---
+++ src/Codewars/JS4/331.js
@@ -0,0 +1,4 @@
+// Implement a function that returns the minimal and the maximal value of a list (in this order).
+
+
+const getMinMax = arr => [Math.min.apply(null, arr), Math.max.apply(null, arr)]

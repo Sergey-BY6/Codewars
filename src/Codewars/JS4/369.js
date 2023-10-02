e87471a080a071796cfcc1530be2bc990cbe8d3f
Sergey Babich <sergei.babich2022@yandex.by>
2023-10-02 14:18:10 +0300

    Add 369 js.

---
+++ src/Codewars/JS4/369.js
@@ -0,0 +1,20 @@
+// In this Kata the aim is to compare each pair of integers from 2 arrays, and return a new array of large numbers.
+//
+//     Note: Both arrays have the same dimensions.
+//
+//     Example:
+//
+// let arr1 = [13, 64, 15, 17, 88];
+// let arr2 = [23, 14, 53, 17, 80];
+// getLargerNumbers(arr1, arr2); // Returns [23, 64, 53, 17, 88]
+
+
+
+const getLargerNumbers = (a, b) => {
+    let result = []
+    for (let i = 0; i < a.length; i++) {
+        a[i] >= b[i] && result.push(a[i])
+        b[i] > a[i] && result.push(b[i])
+    }
+    return result
+}

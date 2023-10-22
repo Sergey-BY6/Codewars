ce1ba0b854e26aa5e83507f2af016bf561b8ab27
Sergey Babich <sergei.babich2022@yandex.by>
2023-10-22 17:26:25 +0300

    Add 382 js.

--- src/Codewars/JS4/382.js
+++ src/Codewars/JS4/382.js
@@ -22,8 +22,8 @@ const catchSignChange = arr => {
     let a = 0
     for (let i = 0; i < arr.length; i++) {
 
-        if (arr[i] >= 0 && arr[i + 1] < 0) a += 1
-        if (arr[i] < 0 && arr[i + 1] >= 0) a += 1
+        if (arr[i] >= 0 && arr[i + 1] < 0) a ++
+        if (arr[i] < 0 && arr[i + 1] >= 0) a ++
     }
     return a
 }

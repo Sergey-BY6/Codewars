c5dab7fc0510aac22b27fa9ce07c02ef6fe8ab0b
Sergey Babich <sergei.babich2022@yandex.by>
2023-10-22 17:25:11 +0300

    Add 382 js.

---
+++ src/Codewars/JS4/382.js
@@ -0,0 +1,29 @@
+// Count how often sign changes in array.
+//
+//     result
+// number from 0 to ... . Empty array returns 0
+//
+// example
+// const arr = [1, -3, -4, 0, 5];
+//
+// /*
+// | elem | count |
+// |------|-------|
+// |  1   |  0    |
+// | -3   |  1    |
+// | -4   |  1    |
+// |  0   |  2    |
+// |  5   |  2    |
+// */
+//
+// catchSignChange(arr) == 2;
+
+const catchSignChange = arr => {
+    let a = 0
+    for (let i = 0; i < arr.length; i++) {
+
+        if (arr[i] >= 0 && arr[i + 1] < 0) a += 1
+        if (arr[i] < 0 && arr[i + 1] >= 0) a += 1
+    }
+    return a
+}

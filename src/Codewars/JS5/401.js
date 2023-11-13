daa09f93553c4d7c254c75f077c40b11c2e8dd1a
Sergey Babich <sergei.babich2022@yandex.by>
2023-11-13 17:32:48 +0300

    Add 401 js.

---
+++ src/Codewars/JS5/401.js
@@ -0,0 +1,13 @@
+// Mash 2 arrays together so that the returning array has alternating elements of the 2 arrays . Both arrays will always be the same length.
+//
+//     eg. [1,2,3] + ['a','b','c'] = [1, 'a', 2, 'b', 3, 'c']
+
+
+const arrayMash = (array1, array2) => {
+    let a = []
+    array1.forEach((el, index) => {
+        a.push(el)
+        a.push(array2[index])
+    })
+    return a
+}
